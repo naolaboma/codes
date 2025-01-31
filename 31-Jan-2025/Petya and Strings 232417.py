a9237f415d604ebd026023948a839ebf94d674c5
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-01-31 22:40:44 +0300

    Created 'Petya and Strings' by A2SV Hub

---
+++ 31-Jan-2025/Petya and Strings 232417.py
@@ -0,0 +1,12 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+x = input()
+y = input()
+x = x.lower()
+y = y.lower()
+if x<y:
+    print(-1)
+elif x==y:
+    print(0)
+else:
+    print(1)
