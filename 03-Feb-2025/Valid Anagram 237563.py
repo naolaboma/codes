df0b4c95708024b7f252c3a3dacc2b033bcf9022
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-03 22:37:40 +0300

    Created 'Valid Anagram' by A2SV Hub

---
+++ 03-Feb-2025/Valid Anagram 237563.py
@@ -0,0 +1,7 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = sorted(s)
+        t = sorted(t)
+        return s==t
