c32fe7eea16a4d52e69ee5682eaaec192ae78ba4
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-03 22:47:36 +0300

    Created 'Missing Number' by A2SV Hub

---
+++ 03-Feb-2025/Missing Number 237585.py
@@ -0,0 +1,8 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        tsum = n*(n + 1)//2
+        csum = sum(nums)
+        return tsum - csum
