201c637ee396dd3972748e26fd0f25f12b821c4a
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-05 15:38:36 +0300

    Created 'Minimum Index Sum of Two Lists' by A2SV Hub

---
+++ 05-Feb-2025/Minimum Index Sum of Two Lists 243094.py
@@ -0,0 +1,14 @@
+# Problem: Minimum Index Sum of Two Lists - https://leetcode.com/problems/minimum-index-sum-of-two-lists/description/
+
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        c1 = {}
+        for i in range(len(list1)):
+            for j in range(len(list2)):
+                if list1[i] == list2[j]:
+                    c1[list1[i]] = i+j
+        res=[]
+        for k, v in c1.items():
+            if v == min(c1.values()):
+                res.append(k)
+        return res
