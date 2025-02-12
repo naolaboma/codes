d999479b4b07d43971a98416df753d081067817b
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-12 12:02:54 +0300

    Created 'Number of smaller' by A2SV Hub

---
+++ 12-Feb-2025/Number of smaller 259221.py
@@ -0,0 +1,12 @@
+# Problem: Number of smaller - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/B
+
+n, m = map(int,input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+res = []
+count=0
+for i in range(m):
+    while count < len(a) and a[count] < b[i]:
+        count+=1
+    res.append(count)
+print(" ".join(map(str, res)))
