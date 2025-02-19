ef3e0df591a0bcf84115442576f4ea1a7f9f4969
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-19 20:38:49 +0300

    Created 'Books' by A2SV Hub

---
+++ 19-Feb-2025/Books 274969.py
@@ -0,0 +1,16 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+def max_books(n, t, books):
+    start = 0
+    current_time = 0
+    max_books = 0
+    for end in range(n):
+        current_time += books[end]
+        while current_time > t:
+            current_time -= books[start]
+            start += 1
+        max_books = max(max_books, end - start + 1)
+    return max_books
+n, t = map(int, input().split())
+books = list(map(int, input().split()))
+print(max_books(n, t, books))
