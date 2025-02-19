7087b7798398eba5c72c56d3cb80c01c5ec29ca8
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-19 18:18:54 +0300

    Created 'Shifting Letters II' by A2SV Hub

---
+++ 19-Feb-2025/Shifting Letters II 274598.py
@@ -0,0 +1,20 @@
+# Problem: Shifting Letters II - https://leetcode.com/problems/shifting-letters-ii/description/
+
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        s = list(s)
+        n = len(s)
+        opera = [0] * (n + 1)
+
+        for start, end, direction in shifts:
+            if direction == 1:
+                opera[start] += 1
+                opera[end + 1] -= 1
+            elif direction == 0:
+                opera[start] -= 1
+                opera[end + 1] += 1
+        for i in range(1, n):
+            opera[i] += opera[i - 1]
+        for j in range(n):
+            s[j] = chr((ord(s[j]) - 97 + opera[j]) % 26 + 97)
+        return "".join(s)
