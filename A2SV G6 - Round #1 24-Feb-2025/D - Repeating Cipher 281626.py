92559b94c24da852c577e6d0ddf0069a04086499
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-24 15:36:25 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 24-Feb-2025/D - Repeating Cipher 281626.py
@@ -0,0 +1,11 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n = int(input())
+t = str(input())
+st = ""
+l, r = 0, 0
+while r < n:
+    st+=t[r]
+    l+=1
+    r+=l
+print(st)
