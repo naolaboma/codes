94fad9d075ab117cf38bbe5ffe67260b73436027
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-24 15:17:05 +0300

    Created 'C - Compare T-Shirt Sizes' by A2SV Hub

---
+++ A2SV G6 - Round #1 24-Feb-2025/C - Compare T-Shirt Sizes 281613.py
@@ -0,0 +1,36 @@
+# Problem: C - Compare T-Shirt Sizes - https://codeforces.com/gym/585107/problem/C
+
+t = int(input())
+results = []
+for _ in range(t):
+    a, b = input().split()
+    type_a = a[-1]
+    type_b = b[-1]
+    
+    if type_a != type_b:
+        if type_a == 'S':
+            results.append('<')
+        elif type_a == 'M' and type_b == 'S':
+            results.append('>')
+        elif type_a == 'M' and type_b == 'L':
+            results.append('<')
+        elif type_a == 'L':
+            results.append('>')
+    else:
+        if type_a == 'M':
+            results.append('=')
+        elif type_a == 'S':
+            if len(a) == len(b):
+                results.append('=')
+            elif len(a) < len(b):
+                results.append('>')
+            else:
+                results.append('<')
+        elif type_a == 'L':
+            if len(a) == len(b):
+                results.append('=')
+            elif len(a) < len(b):
+                results.append('<')
+            else:
+                results.append('>')
+print("\n".join(results))
