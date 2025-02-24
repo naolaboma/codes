b39de99a200d93443ee11ea36eef252b21b68e33
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-24 14:28:33 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 24-Feb-2025/E - Equalizing Arrays 281568.py
@@ -0,0 +1,39 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+def max_equal_length(n, a, m, b):
+    if sum(a) != sum(b):
+        return -1
+    i, j = 0, 0
+    count = 0
+    current_a, current_b = 0, 0
+    while i < n and j < m:
+        if current_a == 0 and current_b == 0:
+            current_a += a[i]
+            current_b += b[j]
+            i += 1
+            j += 1
+        elif current_a < current_b:
+            current_a += a[i]
+            i += 1
+        elif current_a > current_b:
+            current_b += b[j]
+            j += 1
+        else:
+            count += 1
+            current_a, current_b = 0, 0
+    while i < n:
+        current_a += a[i]
+        i += 1
+    while j < m:
+        current_b += b[j]
+        j += 1
+    if current_a == current_b:
+        count += 1
+    else:
+        return -1
+    return count
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+print(max_equal_length(n, a, m, b))
