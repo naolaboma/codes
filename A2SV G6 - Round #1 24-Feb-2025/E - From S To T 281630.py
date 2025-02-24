fe9fe6ae60e546da20b625117d4f0f99c45cc648
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-24 15:45:46 +0300

    Created 'E - From S To T' by A2SV Hub

---
+++ A2SV G6 - Round #1 24-Feb-2025/E - From S To T 281630.py
@@ -0,0 +1,24 @@
+# Problem: E - From S To T - https://codeforces.com/gym/585107/problem/E
+
+def can_transform(s, t, p):
+    from collections import Counter
+    if not is_subsequence(s, t):
+        return False
+    freq_s = Counter(s)
+    freq_t = Counter(t)
+    freq_p = Counter(p)
+    for char in freq_t:
+        if freq_t[char] > freq_s[char] + freq_p[char]:
+            return False
+    return True
+def is_subsequence(s, t):
+    it = iter(t)
+    return all(c in it for c in s)
+q = int(input())
+results = []
+for _ in range(q):
+    s = input()
+    t = input()
+    p = input()
+    results.append("YES" if can_transform(s, t, p) else "NO")
+print("\n".join(results))
