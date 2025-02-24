cf92b822eef995ae73018e8f261cbe294c371343
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-02-24 15:55:03 +0300

    Created 'E - Symmetrization' by A2SV Hub

---
+++ A2SV G6 - Round #2 24-Feb-2025/E - Symmetrization 281639.py
@@ -0,0 +1,21 @@
+# Problem: E - Symmetrization - https://codeforces.com/gym/586960/problem/E
+
+def min_operations_to_symmetrize(grid, n):
+    operations = 0
+    for i in range(n):
+        for j in range(n):
+            if i <= j and i < n - 1 - j:
+                a = grid[i][j]
+                b = grid[j][n - 1 - i]
+                c = grid[n - 1 - i][n - 1 - j]
+                d = grid[n - 1 - j][i]
+                ones = int(a) + int(b) + int(c) + int(d)
+                operations += min(ones, 4 - ones)
+    return operations
+t = int(input())
+results = []
+for _ in range(t):
+    n = int(input())
+    grid = [input().strip() for _ in range(n)]
+    results.append(min_operations_to_symmetrize(grid, n))
+print("\n".join(map(str, results)))
