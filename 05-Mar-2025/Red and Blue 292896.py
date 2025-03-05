803d1eaa7293f20107bc4c74398577c7fbcf7f60
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-03-05 16:14:10 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 05-Mar-2025/Red and Blue 292896.py
@@ -0,0 +1,19 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    r = list(map(int, input().split()))
+    m = int(input())
+    b = list(map(int, input().split()))
+    pres1 = 0
+    pres2 = 0
+    maxi = 0
+    maxj = 0
+    for i in range(n):
+        pres1 += r[i]
+        maxi= max(maxi, pres1)
+    for j in range(m):
+        pres2+=b[j]
+        maxj = max(maxj, pres2)
+    print(maxi + maxj)
