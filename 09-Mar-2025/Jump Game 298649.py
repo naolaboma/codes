a004772db2a88b6ce73a750d92faaa735e15f664
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-03-09 17:20:44 +0300

    Created 'Jump Game' by A2SV Hub

---
+++ 09-Mar-2025/Jump Game 298649.py
@@ -0,0 +1,12 @@
+# Problem: Jump Game - https://leetcode.com/problems/jump-game/
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        win = n - 1  
+        if n == 1:
+            return True  
+        for i in range(n - 2, -1, -1):
+            if win - i <= nums[i]:  
+                win = i   
+        return win == 0
