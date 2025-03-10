bb64196560e33b38f4c73a529b1525459daca11a
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-03-10 21:37:27 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 10-Mar-2025/B - Fix the Forecast! 300679.py
@@ -0,0 +1,14 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())  
+    a = list(map(int, input().split()))  
+    b = list(map(int, input().split()))  
+    
+    a_sorted = sorted((val, idx) for idx, val in enumerate(a))  
+    b.sort()  
+    res = [0] * n 
+    for (val, idx), temp in zip(a_sorted, b):
+        res[idx] = temp  
+    print(*res)  
