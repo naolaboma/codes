107fdd17399e1fef6d5275fe372b1b6f21b4615a
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-03-12 18:49:46 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 12-Mar-2025/Kuriyama Mirai's Stones 304134.py
@@ -0,0 +1,21 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+n = int(input())
+intt = list(map(int, input().split()))
+m = int(input())
+pro = [0] * (n + 1)
+for i in range(1, n + 1):
+    pro[i] = pro[i - 1] + intt[i - 1]
+pfs = [0] * (n + 1)
+sint = sorted(intt)
+
+
+for i in range(1, n + 1):
+    pfs[i] = pfs[i - 1] + sint[i - 1]
+
+for _ in range(m):
+    t, l, r = map(int, input().split())
+    if t == 1:
+        print(pro[r] - pro[l - 1])
+    elif t == 2:
+        print(pfs[r] - pfs[l - 1])
