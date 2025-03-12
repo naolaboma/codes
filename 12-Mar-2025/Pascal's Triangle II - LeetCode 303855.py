54bdf5eb620538ac60ed9e25b961c28edb5db6d9
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-03-12 16:09:13 +0300

    Created 'Pascal's Triangle II - LeetCode' by A2SV Hub

---
+++ 12-Mar-2025/Pascal's Triangle II - LeetCode 303855.py
@@ -0,0 +1,5 @@
+# Problem: Pascal's Triangle II - LeetCode - https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        return [comb(rowIndex, k) for k in range(rowIndex + 1)]
