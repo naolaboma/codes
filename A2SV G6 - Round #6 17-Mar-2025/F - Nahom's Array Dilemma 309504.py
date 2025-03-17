9f08055f454f4a80f23d7d0d8db4657c2badaaba
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-03-17 19:40:07 +0300

    Created 'F - Nahom's Array Dilemma' by A2SV Hub

---
+++ A2SV G6 - Round #6 17-Mar-2025/F - Nahom's Array Dilemma 309504.py
@@ -0,0 +1,23 @@
+# Problem: F - Nahom's Array Dilemma - https://codeforces.com/gym/594077/problem/F
+
+def solve():
+    n = int(input())
+    nums = list(map(int, input().split()))
+    def prevGreater(nums):
+        stack = []
+        pref = [0]
+        for num in nums:
+            pref.append(pref[-1] + num)
+        for i in range(n):
+            while stack and nums[stack[-1]] <= nums[i]:
+                v = stack.pop()
+                if pref[i] - pref[v] > 0:
+                    return False
+            stack.append(i)
+        return True
+    if prevGreater(nums) and prevGreater(nums[::-1]):
+        return "YES"
+    return "NO"
+t = int(input())
+for _ in range(t):
+    print(solve())
