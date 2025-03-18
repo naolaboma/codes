32708d95f9bc3856d86230d0f0ed2f3c5c146272
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-03-18 19:01:17 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 18-Mar-2025/Array Splitting 311443.py
@@ -0,0 +1,10 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+def min_cost_split(n, k, arr):
+    diffs = [arr[i] - arr[i - 1] for i in range(1, n)]
+    diffs.sort(reverse=True)
+    return arr[-1] - arr[0] - sum(diffs[:k-1])
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+print(min_cost_split(n, k, arr))
