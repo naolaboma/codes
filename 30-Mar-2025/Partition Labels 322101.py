37a9df584c006cec55ee58332b4ea0c4b1c6d212
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-03-30 21:19:07 +0300

    Created 'Partition Labels' by A2SV Hub

---
+++ 30-Mar-2025/Partition Labels 322101.py
@@ -0,0 +1,17 @@
+# Problem: Partition Labels - https://leetcode.com/problems/partition-labels/
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        c1 = defaultdict(str)
+        for i in range(len(s)):
+            c1[s[i]]=i
+        size,end = 0,0
+        res = []
+        
+        for i in range(len(s)):
+            size+=1
+            end = max(end,c1.get(s[i]))
+            if i == end:
+                res.append(size)
+                size = 0
+        return res
