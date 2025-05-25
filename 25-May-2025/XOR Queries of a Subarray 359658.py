ace15ac4a2a3f9e8476ebf390397c2eac322a39e
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-05-25 15:11:52 +0300

    Created 'XOR Queries of a Subarray' by A2SV Hub

---
+++ 25-May-2025/XOR Queries of a Subarray 359658.py
@@ -0,0 +1,13 @@
+# Problem: XOR Queries of a Subarray - https://leetcode.com/problems/xor-queries-of-a-subarray/
+
+class Solution:
+    def xorQueries(self, arr, queries):
+        result = []
+        for i in range(1, len(arr)):
+            arr[i] ^= arr[i - 1]
+        for left, right in queries:
+            if left > 0:
+                result.append(arr[left - 1] ^ arr[right])
+            else:
+                result.append(arr[right])
+        return result
