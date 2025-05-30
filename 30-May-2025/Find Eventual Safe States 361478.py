2e4892f9057828390c44dcd14677790117381fb7
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-05-30 23:23:56 +0300

    Created 'Find Eventual Safe States' by A2SV Hub

---
+++ 30-May-2025/Find Eventual Safe States 361478.py
@@ -0,0 +1,30 @@
+# Problem: Find Eventual Safe States - https://leetcode.com/problems/find-eventual-safe-states/
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        indegree = [0] * n
+        adj = [[] for _ in range(n)]
+        for i in range(n):
+            for node in graph[i]:
+                adj[node].append(i)
+                indegree[i] += 1
+        q = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+        safe = [False] * n
+        while q:
+            node = q.popleft()
+            safe[node] = True
+
+            for neighbor in adj[node]:
+                # Delete the edge "node -> neighbor".
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+        safeNodes = []
+        for i in range(n):
+            if safe[i]:
+                safeNodes.append(i)
+        return safeNodes
