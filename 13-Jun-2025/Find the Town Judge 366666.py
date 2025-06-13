28444665e77a8b8efc401a2383366f5b72c3f8c0
Naol Aboma <153062988+naolaboma@users.noreply.github.com>
2025-06-13 23:38:41 +0300

    Created 'Find the Town Judge' by A2SV Hub

---
+++ 13-Jun-2025/Find the Town Judge 366666.py
@@ -0,0 +1,14 @@
+# Problem: Find the Town Judge - https://leetcode.com/problems/find-the-town-judge/
+
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        ind = [0] * (N + 1)
+        otd = [0] * (N + 1)
+        
+        for a in trust:
+            otd[a[0]] += 1
+            ind[a[1]] += 1
+        for i in range(1, N + 1):
+            if ind[i] == N - 1 and otd[i] == 0:
+                return i
+        return -1
