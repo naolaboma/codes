28cf8b15a125b7344b2cdcd6717d3f428cd4db96
Naol Aboma <naolaboma@gmail.com>
2025-11-25 23:05:54 +0300

    Created 'Plus One' by A2SV Hub

---
+++ 25-Nov-2025/Plus One 411299.py
@@ -0,0 +1,22 @@
+# Problem: Plus One - https://leetcode.com/problems/plus-one/
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if digits == [9]:
+            return [1, 0]
+        n = len(digits)
+        c = 0
+        p = n-1
+        while p >= 0:
+            if digits[p]==9:
+                digits[p] = 0
+                c=1
+                p-=1
+            else:
+                digits[p]+=1
+                c-=1
+                break
+        print(c)
+        if c > 0:
+            digits.insert(0, c)
+        return digits
