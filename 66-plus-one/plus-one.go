37728d7f2e49faa9b6f06d5d5a3f844f6b0e9ac5
Naol Aboma <naolaboma@gmail.com>
2026-01-01 10:38:21 +0300

    Time: 0 ms (100.00%) | Memory: 4 MB (77.23%) - LeetSync

---
+++ 66-plus-one/plus-one.go
@@ -0,0 +1,18 @@
+func plusOne(digits []int) []int {
+    c := 1
+    for i := len(digits)-1; i > -1; i--{
+        digits[i] += 1
+        c = 0
+        if digits[i] > 9{
+            c = 1
+            digits[i] = 0
+        } else{
+            return digits
+        }
+    }
+    if c == 1 {
+        digits = append([]int{1}, digits...)
+    }
+    return digits
+
+}
