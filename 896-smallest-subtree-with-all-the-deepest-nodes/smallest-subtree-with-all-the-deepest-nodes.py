b5b33e25df9bd6a704b817865689b6eda382e52d
Naol Aboma <naolaboma@gmail.com>
2026-01-09 21:43:00 +0300

    Time: 6 ms (1.08%) | Memory: 19.8 MB (11.50%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.py
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.py
@@ -1,27 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def subtreeWithAllDeepest(self, root):
-        # Tag each node with it's depth.
-        depth = {None: -1}
-        def dfs(node, parent = None):
-            if node:
-                depth[node] = depth[parent] + 1
-                dfs(node.left, node)
-                dfs(node.right, node)
-        dfs(root)
+        Result = collections.namedtuple("Result", ("node", "dist"))
+        def dfs(node):
+            if not node: return Result(None, 0)
+            L, R = dfs(node.left), dfs(node.right)
+            if L.dist > R.dist: return Result(L.node, L.dist + 1)
+            if L.dist < R.dist: return Result(R.node, R.dist + 1)
+            return Result(node, L.dist + 1)
 
-        max_depth = max(depth.values())
-
-        def answer(node):
-            # Return the answer for the subtree at node.
-            if not node or depth.get(node, None) == max_depth:
-                return node
-            L, R = answer(node.left), answer(node.right)
-            return node if L and R else L or R
-
-        return answer(root)
+        return dfs(root).node
