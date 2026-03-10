abae888991467bf083509aad12883e9856f8b73e
Naol Aboma <naolaboma@gmail.com>
2026-03-10 15:40:20 +0300

    Time: 10 ms (96.48%) | Memory: 9.8 MB (97.05%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.go
+++ 217-contains-duplicate/contains-duplicate.go
@@ -1,9 +1,4 @@
 func containsDuplicate(nums []int) bool {
-    for i := 0; i < len(nums)-1; i++{
-        if ok:= nums[i]== nums[i+1]; ok{
-            return true
-        }
-    }
     slices.Sort(nums)
 
     for i := 0; i <len(nums)-1; i++{
