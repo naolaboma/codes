3b5f0b9217c488186b99b99b67ab5111c533ea1e
Naol Aboma <naolaboma@gmail.com>
2026-03-12 09:52:01 +0300

    Time: 86 ms (100.00%) | Memory: 23.7 MB (60.00%) - LeetSync

--- 3902-maximize-spanning-tree-stability-with-upgrades/maximize-spanning-tree-stability-with-upgrades.go
+++ 3902-maximize-spanning-tree-stability-with-upgrades/maximize-spanning-tree-stability-with-upgrades.go
@@ -1,111 +1,125 @@
-const MAX_STABILITY = 200000
 
-type DSU struct {
-	parent []int
+import (
+	"math"
+	"slices"
+)
+type unionFind struct {
+	size []int
+	p    []int
 }
 
-func NewDSU(parent []int) *DSU {
-	p := make([]int, len(parent))
-	copy(p, parent)
-	return &DSU{parent: p}
-}
-
-func (d *DSU) find(x int) int {
-	if d.parent[x] != x {
-		d.parent[x] = d.find(d.parent[x])
+func newUnionFind(n int) unionFind {
+	u := unionFind{
+		size: make([]int, n),
+		p:    make([]int, n),
 	}
-	return d.parent[x]
+	for i := 0; i < n; i++ {
+		u.p[i] = i
+		u.size[i] = 1
+	}
+	return u
 }
 
-func (d *DSU) join(x, y int) {
-	px := d.find(x)
-	py := d.find(y)
-	d.parent[px] = py
+func (u *unionFind) find(x int) int {
+	if x >= len(u.p) {
+		return -1
+	}
+	if u.p[x] == x {
+		return x
+	}
+	return u.find(u.p[x])
+}
+
+func (u *unionFind) union(s1, s2 int) {
+	r1 := u.find(s1)
+	r2 := u.find(s2)
+	if r1 == r2 {
+		return
+	}
+	if u.size[r1] > u.size[r2] {
+		u.p[r2] = r1
+		u.size[r1] = u.size[r1] + u.size[r2]
+	} else {
+		u.p[r1] = r2
+		u.size[r2] = u.size[r1] + u.size[r2]
+	}
+}
+
+func (u *unionFind) same(x, y int) bool {
+	return u.find(x) == u.find(y)
 }
 
 func maxStability(n int, edges [][]int, k int) int {
-	ans := -1
+    // sort
+    slices.SortFunc(edges, func(a, b []int) int{
+        if a[3] == 1 {
+            return -1
+        } else if b[3] == 1 {
+            return 1
+        } else if a[3] == 1 && b[3] == 1 {
+            return 0
+        } else {
+            if a[2] < b[2] {
+                return 1
+            } else if a[2] > b[2] {
+                return -1
+            } else {
+                return 0
+            }
+        }
+    })
+    // init union find
+    un := newUnionFind(n)
 
-	if len(edges) < n-1 {
-		return -1
-	}
-	mustEdges := [][]int{}
-	optionalEdges := [][]int{}
+    // kruskal
 
-	for _, e := range edges {
-		if e[3] == 1 {
-			mustEdges = append(mustEdges, e)
-		} else {
-			optionalEdges = append(optionalEdges, e)
-		}
-	}
+    strengths := make([]int, 0, len(edges))
+    cycle := false
+    musts := 0
 
-	if len(mustEdges) > n-1 {
-		return -1
-	}
+    for i := 0; i < len(edges); i++ {
+        u := edges[i][0]
+        v := edges[i][1]
+        s := edges[i][2]
+        must := edges[i][3]
+        if must == 1 {
+            if un.same(u, v) {
+                cycle = true
+                break // cycle found
+            }
+            un.union(u, v)
+            strengths = append(strengths, s)
+            musts++
+        } else {
+            if !un.same(u, v) {
+                un.union(u, v)
+                strengths = append(strengths, s)
+            }
+        }
+    }
 
-	sort.Slice(optionalEdges, func(i, j int) bool {
-		return optionalEdges[i][2] > optionalEdges[j][2]
-	})
+    if cycle { // no cycles
+        return -1
+    }
 
-	selectedInit := 0
-	mustMinStability := MAX_STABILITY
+    p := un.find(0)
+    for i := 1; i < n ; i++ { // everything connected
+        if un.find(i) != p {
+            return -1
+        }
+    }
 
-	initParent := make([]int, n)
-	for i := 0; i < n; i++ {
-		initParent[i] = i
-	}
-	dsuInit := NewDSU(initParent)
-
-	for _, e := range mustEdges {
-		u, v, s := e[0], e[1], e[2]
-		if dsuInit.find(u) == dsuInit.find(v) || selectedInit == n-1 {
-			return -1
-		}
-		dsuInit.join(u, v)
-		selectedInit++
-		if s < mustMinStability {
-			mustMinStability = s
-		}
-	}
-
-	l, r := 0, mustMinStability
-	for l < r {
-		mid := l + (r-l+1)/2
-
-		dsu := NewDSU(dsuInit.parent)
-		selected := selectedInit
-		doubledCount := 0
-
-		for _, e := range optionalEdges {
-			u, v, s := e[0], e[1], e[2]
-			if dsu.find(u) == dsu.find(v) {
-				continue
-			}
-
-			if s >= mid {
-				dsu.join(u, v)
-				selected++
-			} else if doubledCount < k && s*2 >= mid {
-				doubledCount++
-				dsu.join(u, v)
-				selected++
-			} else {
-				break
-			}
-
-			if selected == n-1 {
-				break
-			}
-		}
-
-		if selected != n-1 {
-			r = mid - 1
-		} else {
-			ans = mid
-			l = mid
-		}
-	}
-
-	return ans
+    min := math.MaxInt
+    upgrades := 0
+    for i := len(strengths)-1; i >= 0; i-- {
+        s := strengths[i]
+        if i >= musts && upgrades < k {
+            s = s*2
+            upgrades++
+        }
+        if min > s {
+            min = s
+        }
+    }
+    return min
 }
