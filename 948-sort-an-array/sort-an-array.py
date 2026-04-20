5eb1513f948bedebcf2c1db09c11f6e19b6e642f
Naol Aboma <naolaboma@gmail.com>
2026-04-20 23:06:18 +0300

    Time: 940 ms (9.45%) | Memory: 26.5 MB (81.84%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,35 +1,21 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        temp = [0] * len(nums)
-        self.mergesort(nums,temp, 0, len(nums)-1)
+        self.heapsort(nums, len(nums))
         return nums
-    def merge(self, nums, temp, lb, mid, ub):
-        i = lb
-        j = mid + 1
-        k = lb
-        while i <= mid and j <= ub:
-            if nums[i] <= nums[j]:
-                temp[k] = nums[i]
-                i+=1
-            else:
-                temp[k] = nums[j]
-                j+=1
-            k+=1
-        if i > mid:
-            while j <= ub:
-                temp[k] = nums[j]
-                j+=1
-                k+=1
-        else:
-            while i <= mid:
-                temp[k] = nums[i]
-                i+=1
-                k+=1
-        for x in range(lb, ub+1):
-            nums[x] = temp[x]
-    def mergesort(self, nums, temp, lb, ub):
-        if lb < ub:
-            mid = (lb + ub)//2
-            self.mergesort(nums, temp, lb, mid)
-            self.mergesort(nums, temp, mid+1, ub)
-            self.merge(nums, temp, lb, mid, ub)
+    def maxheapify(self, nums, n, i):
+        largest = i
+        l = 2*i + 1
+        r = 2*i + 2
+        if l < n and nums[l] > nums[largest]:
+            largest = l
+        if r < n and nums[r] > nums[largest]:
+            largest = r
+        if largest != i:
+            nums[i], nums[largest] = nums[largest], nums[i]
+            self.maxheapify(nums, n, largest)
+    def heapsort(self, nums, n):
+        for i in range(n//2-1, -1, -1):
+            self.maxheapify(nums, n, i)
+        for i in range(n-1, 0, -1):
+            nums[0], nums[i] = nums[i], nums[0]
+            self.maxheapify(nums, i, 0)
