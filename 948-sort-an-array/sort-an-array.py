058309fb6b9cda3ab5d9f191bf28ce62d23c1781
Naol Aboma <naolaboma@gmail.com>
2026-04-21 12:53:36 +0300

    Time: 105 ms (82.15%) | Memory: 26.7 MB (73.31%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,21 +1,22 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        self.heapsort(nums, len(nums))
+        OFFSET = 50000  # shift negatives to zero index
+        SIZE = 100001   # from -50000 to 50000 inclusive
+
+        count = [0] * SIZE
+
+        # count frequencies
+        for num in nums:
+            count[num + OFFSET] += 1
+
+        # rebuild sorted array
+        i = 0
+        for val in range(SIZE):
+            freq = count[val]
+            if freq:
+                actual_val = val - OFFSET
+                for _ in range(freq):
+                    nums[i] = actual_val
+                    i += 1
+
         return nums
-    def maxheapify(self, nums, n, i):
-        largest = i
-        l = 2*i + 1
-        r = 2*i + 2
-        if l < n and nums[l] > nums[largest]:
-            largest = l
-        if r < n and nums[r] > nums[largest]:
-            largest = r
-        if largest != i:
-            nums[i], nums[largest] = nums[largest], nums[i]
-            self.maxheapify(nums, n, largest)
-    def heapsort(self, nums, n):
-        for i in range(n//2-1, -1, -1):
-            self.maxheapify(nums, n, i)
-        for i in range(n-1, 0, -1):
-            nums[0], nums[i] = nums[i], nums[0]
-            self.maxheapify(nums, i, 0)
