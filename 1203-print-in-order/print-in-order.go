f805fda2ce8c6b6f7af88f6875f1c19b083eaffa
Naol Aboma <naolaboma@gmail.com>
2026-07-29 11:27:22 +0300

    Time: 0 ms (100.00%) | Memory: 4.1 MB (97.96%) - LeetSync

---
+++ 1203-print-in-order/print-in-order.go
@@ -0,0 +1,33 @@
+type Foo struct {
+    firstDone chan struct{}
+    secondDone chan struct{}
+
+}
+
+func NewFoo() *Foo {
+	return &Foo{
+        firstDone: make(chan struct{}, 1),
+        secondDone: make(chan struct{}, 1),
+	}
+}
+
+func (f *Foo) First(printFirst func()) {
+	// Do not change this line
+	printFirst()
+    f.firstDone <- struct{}{}
+}
+
+func (f *Foo) Second(printSecond func()) {
+	/// Do not change this line
+    <-f.firstDone
+	printSecond()
+
+    f.secondDone <- struct{}{}
+
+}
+
+func (f *Foo) Third(printThird func()) {
+	// Do not change this line
+    <-f.secondDone
+	printThird()
+}
